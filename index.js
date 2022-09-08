e81a6c4471ea1284bb934acc5587e80f22872c9a
Misha Tuesday <mishatuesday@gmail.com>
2022-09-08 12:19:10 -0400

    commit tree db06f86b9eee49ffbb800519ee76b147fdf30cfe

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const clickHere = document.getElementById('main');
+    clickHere.addEventListener('click', clickAlert);
+    return true;
 }
+
+function clickAlert() {
+    alert('I was clicked!');
+}
+
+addingEventListener();
